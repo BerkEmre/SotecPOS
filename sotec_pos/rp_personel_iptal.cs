18e037e3d044a89f77cfc10c09e861b6ca120acb
Berk Emre ALTAN <berkemrealtan@outlook.com>
2020-06-30 00:23:18 +0300

    wep api güncellemeleri

--- sotec_pos/rp_personel_iptal.cs
+++ sotec_pos/rp_personel_iptal.cs
@@ -14,7 +14,7 @@ namespace sotec_pos
             InitializeComponent();
 
             lbl_tarih.Text = tarih1.Day + "." + tarih1.Month + "." + tarih1.Year + "-" + tarih2.Day + "." + tarih2.Month + "." + tarih2.Year;
-            DataTable dt = SQL.get("SELECT ad_soyad = k.ad + ' ' + k.soyad, u.urun_adi, miktar = SUM(ak.miktar), tutar = SUM(ak.miktar * u.fiyat) FROM kullanicilar k LEFT OUTER JOIN adisyon_kalem ak ON ak.silindi = 1 AND ak.kaydeden_kullanici_id = k.kullanici_id AND ak.kayit_tarihi BETWEEN '" + tarih1.ToString("yyyy-MM-dd HH:mm:00.000") + "' AND DATEADD(DAY, 1, '" + tarih2.ToString("yyyy-MM-dd HH:mm:00.000") + "') INNER JOIN urunler u ON u.urun_id = ak.urun_id " +
+            DataTable dt = SQL.get("SELECT ad_soyad = k.ad + ' ' + k.soyad, u.urun_adi, miktar = SUM(ak.miktar), tutar = SUM(ak.miktar * u.fiyat) FROM kullanicilar k LEFT OUTER JOIN adisyon_kalem ak ON ak.silindi = 1 AND ak.durum_parametre_id != 51 AND ak.kaydeden_kullanici_id = k.kullanici_id AND ak.kayit_tarihi BETWEEN '" + tarih1.ToString("yyyy-MM-dd HH:mm:00.000") + "' AND DATEADD(DAY, 1, '" + tarih2.ToString("yyyy-MM-dd HH:mm:00.000") + "') INNER JOIN urunler u ON u.urun_id = ak.urun_id " +
                 " WHERE k.silindi = 0 GROUP by k.ad, k.soyad, u.urun_adi");
 
             this.DataSource = dt;
--- web_api/Controllers/ReportController.cs
+++ web_api/Controllers/ReportController.cs
@@ -61,24 +61,30 @@ namespace web_api.Controllers
                 return RedirectToAction("Index", "Report");
 
             DataTable dt_gun = SQL.get("SELECT TOP 1 * FROM gunler ORDER by gun_id DESC");
-            DataTable dt_gunluk_satis_nakit = SQL.get("SELECT tutar = FORMAT(ISNULL(SUM(miktar), 0), 'N2') FROM finans_hareket WHERE silindi = 0 AND hareket_tipi_parametre_id = 25 AND kayit_tarihi >= (SELECT TOP 1 baslangic_tarihi FROM gunler ORDER by gun_id DESC)");
-            DataTable dt_gunluk_satis_kredi = SQL.get("SELECT tutar = FORMAT(ISNULL(SUM(miktar), 0), 'N2') FROM finans_hareket WHERE silindi = 0 AND hareket_tipi_parametre_id = 26 AND kayit_tarihi >= (SELECT TOP 1 baslangic_tarihi FROM gunler ORDER by gun_id DESC)");
-            DataTable dt_gunluk_satis_yfisi = SQL.get("SELECT tutar = FORMAT(ISNULL(SUM(miktar), 0), 'N2') FROM finans_hareket WHERE silindi = 0 AND hareket_tipi_parametre_id = 27 AND kayit_tarihi >= (SELECT TOP 1 baslangic_tarihi FROM gunler ORDER by gun_id DESC)");
+            DataTable dt_gunluk_satis_nakit = SQL.get("SELECT tutar = ROUND(ISNULL(SUM(miktar), 0), 2) FROM finans_hareket WHERE silindi = 0 AND hareket_tipi_parametre_id = 25 AND kayit_tarihi >= (SELECT TOP 1 baslangic_tarihi FROM gunler ORDER by gun_id DESC)");
+            DataTable dt_gunluk_satis_kredi = SQL.get("SELECT tutar = ROUND(ISNULL(SUM(miktar), 0), 2) FROM finans_hareket WHERE silindi = 0 AND hareket_tipi_parametre_id = 26 AND kayit_tarihi >= (SELECT TOP 1 baslangic_tarihi FROM gunler ORDER by gun_id DESC)");
+            DataTable dt_gunluk_satis_yfisi = SQL.get("SELECT tutar = ROUND(ISNULL(SUM(miktar), 0), 2) FROM finans_hareket WHERE silindi = 0 AND hareket_tipi_parametre_id = 27 AND kayit_tarihi >= (SELECT TOP 1 baslangic_tarihi FROM gunler ORDER by gun_id DESC)");
 
-            DataTable dt_gunluk_urunler = SQL.get("SELECT adet = FORMAT(ISNULL(SUM(miktar), 0), 'N1') FROM adisyon_kalem WHERE silindi = 0 AND kayit_tarihi >= (SELECT TOP 1 baslangic_tarihi FROM gunler ORDER by gun_id DESC)");
+            DataTable dt_gunluk_urunler = SQL.get("SELECT adet = ROUND(ISNULL(SUM(miktar), 0), 1) FROM adisyon_kalem WHERE silindi = 0 AND kayit_tarihi >= (SELECT TOP 1 baslangic_tarihi FROM gunler ORDER by gun_id DESC)");
             DataTable dt_gunluk_adisyon = SQL.get("SELECT adet = ISNULL(COUNT(*), 0) FROM (SELECT adisyon_id FROM adisyon_kalem WHERE silindi = 0 AND kayit_tarihi >= (SELECT TOP 1 baslangic_tarihi FROM gunler ORDER by gun_id DESC) GROUP by adisyon_id) as tbl");
 
-            DataTable dt_gunluk_iptal = SQL.get("SELECT miktar = FORMAT(ISNULL(SUM(miktar), 0), 'N1') FROM adisyon_kalem WHERE silindi = 1 AND kayit_tarihi >= (SELECT TOP 1 baslangic_tarihi FROM gunler ORDER by gun_id DESC)");
-            DataTable dt_gunluk_ikram = SQL.get("SELECT miktar = FORMAT(ISNULL(SUM(ikram_miktar), 0), 'N1') FROM adisyon_kalem WHERE silindi = 0 AND kayit_tarihi >= (SELECT TOP 1 baslangic_tarihi FROM gunler ORDER by gun_id DESC)");
+            DataTable dt_gunluk_iptal = SQL.get("SELECT miktar = ROUND(ISNULL(SUM(miktar), 0), 1) FROM adisyon_kalem WHERE silindi = 1 AND durum_parametre_id != 51 AND kayit_tarihi >= (SELECT TOP 1 baslangic_tarihi FROM gunler ORDER by gun_id DESC)");
+            DataTable dt_gunluk_ikram = SQL.get("SELECT miktar = ROUND(ISNULL(SUM(ikram_miktar), 0), 1) FROM adisyon_kalem WHERE silindi = 0 AND kayit_tarihi >= (SELECT TOP 1 baslangic_tarihi FROM gunler ORDER by gun_id DESC)");
+           
+            DataTable dt_gg_nakit = SQL.get("SELECT tutar = ROUND(ISNULL(SUM(miktar), 0), 2) FROM finans_hareket WHERE silindi = 0 AND hareket_tipi_parametre_id = 60 AND kayit_tarihi >= (SELECT TOP 1 baslangic_tarihi FROM gunler ORDER by gun_id DESC)");
+            DataTable dt_gg_kredi = SQL.get("SELECT tutar = ROUND(ISNULL(SUM(miktar), 0), 2) FROM finans_hareket WHERE silindi = 0 AND hareket_tipi_parametre_id = 61 AND kayit_tarihi >= (SELECT TOP 1 baslangic_tarihi FROM gunler ORDER by gun_id DESC)");
 
             ViewBag.Title = "Genel Bakış";
-            ViewBag.Nakit = dt_gunluk_satis_nakit.Rows[0]["tutar"];
-            ViewBag.Kredi = dt_gunluk_satis_kredi.Rows[0]["tutar"];
-            ViewBag.Yfisi = dt_gunluk_satis_yfisi.Rows[0]["tutar"];
-            ViewBag.Urunler = dt_gunluk_urunler.Rows[0]["adet"];
-            ViewBag.Adisyon = dt_gunluk_adisyon.Rows[0]["adet"];
-            ViewBag.Iptal = dt_gunluk_iptal.Rows[0]["miktar"];
-            ViewBag.Ikram = dt_gunluk_ikram.Rows[0]["miktar"];
+            ViewBag.Nakit = Convert.ToDecimal(dt_gunluk_satis_nakit.Rows[0]["tutar"]).ToString("n2");
+            ViewBag.Kredi = Convert.ToDecimal(dt_gunluk_satis_kredi.Rows[0]["tutar"]).ToString("n2");
+            ViewBag.Yfisi = Convert.ToDecimal(dt_gunluk_satis_yfisi.Rows[0]["tutar"]).ToString("n2");
+            ViewBag.Urunler = Convert.ToDecimal(dt_gunluk_urunler.Rows[0]["adet"]).ToString("n0");
+            ViewBag.Adisyon = Convert.ToDecimal(dt_gunluk_adisyon.Rows[0]["adet"]).ToString("n0");
+            ViewBag.Iptal = Convert.ToDecimal(dt_gunluk_iptal.Rows[0]["miktar"]).ToString("n0");
+            ViewBag.Ikram = Convert.ToDecimal(dt_gunluk_ikram.Rows[0]["miktar"]).ToString("n0");
+            ViewBag.GG = (Convert.ToDecimal(dt_gg_nakit.Rows[0]["tutar"]) + Convert.ToDecimal(dt_gg_kredi.Rows[0]["tutar"])).ToString("n2");
+            ViewBag.Ciro = (Convert.ToDecimal(dt_gunluk_satis_nakit.Rows[0]["tutar"]) + Convert.ToDecimal(dt_gunluk_satis_kredi.Rows[0]["tutar"]) + Convert.ToDecimal(dt_gunluk_satis_yfisi.Rows[0]["tutar"])).ToString("n2");
+            ViewBag.Net = (Convert.ToDecimal(dt_gunluk_satis_nakit.Rows[0]["tutar"]) + Convert.ToDecimal(dt_gunluk_satis_kredi.Rows[0]["tutar"]) + Convert.ToDecimal(dt_gunluk_satis_yfisi.Rows[0]["tutar"]) + Convert.ToDecimal(dt_gg_nakit.Rows[0]["tutar"]) + Convert.ToDecimal(dt_gg_kredi.Rows[0]["tutar"])).ToString("n2");
 
             DataTable dt = SQL.get(
                 "SELECT " +
@@ -86,24 +92,36 @@ namespace web_api.Controllers
                 "    m.masa_adi, " +
                 "    mk.masa_kategori," +
                 "    adisyon_id = ISNULL(a.adisyon_id, 0), " +
-                "    tutar = FORMAT((SELECT SUM((ak.miktar - ak.ikram_miktar) * u.fiyat) FROM adisyon_kalem ak INNER JOIN urunler u ON u.urun_id = ak.urun_id WHERE ak.adisyon_id = a.adisyon_id AND ak.silindi = 0), 'N2'), " +
-                "    sure = DATEDIFF(MINUTE, a.kayit_tarihi, GETDATE()) " +
+                "    tutar = " +
+                "       ISNULL((SELECT SUM((ak.miktar - ak.ikram_miktar) * u.fiyat) FROM adisyon_kalem ak INNER JOIN urunler u ON u.urun_id = ak.urun_id INNER JOIN parametreler p ON p.parametre_id = u.olcu_birimi_parametre_id INNER JOIN parametreler dr ON dr.parametre_id = ak.durum_parametre_id LEFT OUTER JOIN kullanicilar kl ON kl.kullanici_id = ak.kaydeden_kullanici_id WHERE ak.silindi = 0 AND ak.adisyon_id = a.adisyon_id), 0.0000) " +
+                "       - " +
+                "       ISNULL((SELECT SUM(fh.miktar) FROM finans_hareket fh INNER JOIN parametreler p ON p.parametre_id = fh.hareket_tipi_parametre_id WHERE fh.silindi = 0 AND hareket_tipi_parametre_id IN(25, 26, 27, 59) AND referans_id = a.adisyon_id), 0.0000), " +
+                "    sure = DATEDIFF(MINUTE, a.kayit_tarihi, GETDATE()), " +
+                "    acik_mi = CASE ISNULL(a.adisyon_id, 0) WHEN 0 THEN 0 ELSE 1 END " +
                 "FROM " +
                 "    masalar m " +
                 "    INNER JOIN masalar_kategori mk ON mk.masa_kategori_id = m.masa_kategori_id" +
                 "    LEFT OUTER JOIN adisyon a ON a.masa_id = m.masa_id AND a.silindi = 0 AND a.kapandi = 0 " +
                 "WHERE " +
                 "    m.silindi = 0 " +
-                "ORDER by mk.masa_kategori, m.masa_adi");
+                "ORDER by acik_mi DESC, mk.masa_kategori, m.masa_adi");
 
             return View(dt);
         }
 
         public ActionResult toplamSatis(int gun_id = 0, string ilk_tarih = null, string son_tarih = null) {
-            if (Session["kullanici_id"] != null)
-                return RedirectToAction("Dashboard", "Report");
+            if (Session["kullanici_id"] == null)
+                return RedirectToAction("Index", "Report");
 
             return View();
+        }
+
+        public ActionResult raporAl(int gun_id, string baslangic_tarihi, string bitis_tarihi, string rapor_tipi)
+        {
+            if (gun_id != 0 && (baslangic_tarihi == null || bitis_tarihi == null))
+                return RedirectToAction("toplamSatis", "Report", new { hata = "Tarih girin!" });
+
+            return RedirectToAction("toplamSatis", "Report", new { gun_id = gun_id, baslangic_tarihi = baslangic_tarihi, bitis_tarihi = bitis_tarihi, rapor_tipi = rapor_tipi });
         }
     }
 }
